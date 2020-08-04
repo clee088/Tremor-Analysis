0ce946ee1d5cc9b285bc9877231eb4deefccf80c
clee088 <42843045+clee088@users.noreply.github.com>
2020-08-04 13:53:43 -0400

    From Sparkle

---
+++ js/terms-conditions.20200804133708.js
@@ -0,0 +1,14 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+initMenu($('#m1')[0]);
+$('.c18').Stickyfill();
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js3')[0], "0.50s", "0.00s", 1, 100);
+wl.start();
+
+});
